2a66cc056a8b795f54d861dd53ef46c7f8188be5
DryPerspective <41272259+DryPerspective@users.noreply.github.com>
2024-02-21 09:13:30 +0000

    Added SCOPE_SUCCESS and SCOPE_FAIL to C++98 defer

--- include/bits/defer_cpp98.h
+++ include/bits/defer_cpp98.h
@@ -1,6 +1,8 @@
 #ifndef DP_CPP98_DEFER
 #define DP_CPP98_DEFER
 
+#include <exception>
+
 /*
 *  A DEFER tool, also known as a SCOPE_EXIT tool. If a function should contain the expression DEFER(foo) then foo will automatically be executed when the scope exits.
 *  Note that due to the limitations of C++98, this tool is not aware of any function-local data and can only call stateless code or code which manipulates some global state (ugh).
@@ -23,7 +25,31 @@
                     } DEFER_CONCAT_MACRO(Defer_Struct, DEFER_CONCAT_COUNT);
 
 
+
+
 #define SCOPE_EXIT(ARGS) DEFER(ARGS)
+
+#define SCOPE_FAIL(ARGS) struct{\
+                            struct Defer_Impl{ \
+                                Defer_Impl() {} \
+                                ~Defer_Impl(){ \
+                                    if(std::uncaught_exception()){ \
+                                        ARGS ;\
+                                    } \
+                                } \
+                            } impl; \
+                        } DEFER_CONCAT_MACRO(Defer_Struct, DEFER_CONCAT_COUNT);
+
+#define SCOPE_SUCCESS(ARGS) struct{\
+                                struct Defer_Impl{ \
+                                    Defer_Impl() {} \
+                                    ~Defer_Impl(){ \
+                                        if(!std::uncaught_exception()){ \
+                                            ARGS ;\
+                                        } \
+                                    } \
+                                } impl; \
+                        } DEFER_CONCAT_MACRO(Defer_Struct, DEFER_CONCAT_COUNT);
 
 
 #endif
