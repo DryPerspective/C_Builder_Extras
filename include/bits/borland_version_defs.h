1ec37ca1de5b948ab6adfc8e2096d3f6d4c42ba8
DryPerspective <41272259+DryPerspective@users.noreply.github.com>
2024-04-15 13:06:13 +0100

    Rework of dumbass design in version defs

--- include/bits/borland_version_defs.h
+++ include/bits/borland_version_defs.h
@@ -6,16 +6,16 @@
 //Instead we have to do a hacky workaround based on the version number of the *IDE*
 //I don't make the rules.
 #ifdef __BORLANDC__
-#if __BORLANDC__ < 0x552 //Original value is = 0x551 for C++Builder 5
+#if __BORLANDC__ >= 0x551 
 #define DP_CBUILDER5
-#elif __BORLANDC__ < 0x0740
-#define DP_CBUILDER5
+#endif
+#if __BORLANDC__ >= 0x730
 #define DP_CBUILDER10
-#elif __BORLANDC__ < 0x770
-#define DP_CBUILDER5
-#define DP_CBUILDER10
+#endif
+#if __BORLANDC__ >= 0x770
 #define DP_CBUILDER11
 #endif
 #endif
+
 
 #endif
